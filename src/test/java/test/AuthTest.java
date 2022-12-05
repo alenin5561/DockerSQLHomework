9f293deedf78ef602aca94a171db8a67169af8ba
Аленин Андрей <aleninmailbox@gmail.com>
2022-12-05 20:06:17 +0300

    corrections

--- src/test/java/test/AuthTest.java
+++ src/test/java/test/AuthTest.java
@@ -37,7 +37,7 @@ public class AuthTest {
         var authInfo = DataHelper.getInvalidInfo();
         var verificationPage = loginPage.threeTimesInvalidPassword(authInfo);
         var actual = DataHelper.getUserStatus("vasya");
-        var expected = "active";
+        var expected = "blocked";
         Assertions.assertEquals(expected, actual);
     }
 
